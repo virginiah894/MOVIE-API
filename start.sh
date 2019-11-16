3569f34f1be1def6b41e93133a7e9083951d5ad8
virginiah894 <virgyperry@gmail.com>
2019-11-16 10:53:25 +0300

    Deployment to Heroku

---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='2dcfadf092ccf8c7bf638533241dc04e'
+export SECRET_KEY='123456789'
+
+python3.6 manage.py server
---
+++ tests/movie_test.py
@@ -0,0 +1,11 @@
+import unittest
+from app.models import Movie
+
+class Movietest(unittest.TestCase):
+  def setUp(self):
+    self.new_movie = Movie (1234,'Python must be crazy','A thrilling new python series','https://image.tmdb.org/t/p/w500/khsjha27hbs','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5)
+  def test_instance(self):
+    self.assertTrue(isinstance(self.new_movie,Movie))
+if __name__ == '__main__':
+  
+  unittest.main()
